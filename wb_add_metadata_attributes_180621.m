3a2e6eeecabd8e2df2d1d6bf3febf212cb9b9208
Raymond Dunn <ray.l.dunn@gmail.com>
2020-10-12 12:54:02 -0700

    some refactoring

--- wb_add_metadata_attributes_180621.m
+++
@@ -1,4 +0,0 @@
-[metadata, metafields]=wbLoadMetaFile(pwd);
-metadata.fileInfo.numZ = metadata.fileInfoOverride.numZ;
-metadata.fileInfo.numTInFile = metadata.fileInfoOverride.numT;
-save([pwd filesep 'meta.mat'],'-struct','metadata');
